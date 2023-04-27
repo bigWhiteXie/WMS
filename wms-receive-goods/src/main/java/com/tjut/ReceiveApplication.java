0313e423975fa272192648d83cfb705f89655ae1
abc <abc@qq.com>
2023-04-27 10:34:56 +0800

    dev

--- wms-receive-goods/src/main/java/com/tjut/ReceiveApplication.java
+++ wms-receive-goods/src/main/java/com/tjut/ReceiveApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReceiveApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReceiveApplication.class,args);
+        System.out.println("");
     }
 }
